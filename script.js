8cd73ea578854a1b286c1ee574e626c00c96d500
zanoniweb <163794616+zanoniweb@users.noreply.github.com>
2025-02-16 18:48:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
- function formatarInscricao() {
-            const input = document.getElementById('inscricao');
-            let valor = input.value;
-
-            // Remove qualquer caractere que não seja número
-            valor = valor.replace(/\D/g, '');
-
-            // Aplica a máscara XX.XXX.XXX.XXXX.XXX
-            if (valor.length > 2) valor = valor.replace(/^(\d{2})(\d)/, '$1.$2');
-            if (valor.length > 6) valor = valor.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
-            if (valor.length > 9) valor = valor.replace(/^(\d{2})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3.$4');
-            if (valor.length > 13) valor = valor.replace(/^(\d{2})\.(\d{3})\.(\d{3})\.(\d{4})(\d)/, '$1.$2.$3.$4.$5');
-            
-            // Atualiza o valor do campo de entrada
-            input.value = valor;
-        }
-
 async function buscarDados() {
   const inscricao = document.getElementById('search').value; // Correção do ID
   const anos = [2020, 2021, 2022, 2023, 2024, 2025, 2026];
